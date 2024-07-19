c7dbd0f8f9c1a7ac5c4e55d034d5c94438bc92ed
Anas <163267774+anasahhm@users.noreply.github.com>
2024-07-19 23:48:20 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms'; // Import this for reactive forms
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppAddUserComponent } from './add-user/app-add-user.component';
-import { AppUserListComponent } from './user-list/app-user-list.component'; // Import the user list component
+import { AppUserListComponent } from './user-list/app-user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppAddUserComponent,
-    AppUserListComponent // Declare the user list component
+    AppUserListComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule // Add this to the imports array
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
