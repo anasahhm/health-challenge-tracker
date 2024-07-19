6aa3d7e245241f4a7941b9458cbe0ec5d3e4b582
Anas <163267774+anasahhm@users.noreply.github.com>
2024-07-20 00:01:40 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AddUserComponent } from './components/add-user/add-user.component';
@@ -15,7 +15,6 @@ import { UserListComponent } from './components/user-list/user-list.component';
     // other imports
   ],
   providers: [],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
